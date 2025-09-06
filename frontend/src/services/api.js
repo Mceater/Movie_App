df9e435d94321fefd9d94c6dd2ee0d67154b3fab
mceater <farhanrisan@gmail.com>
2025-09-07 02:55:14 +1000

    gitignore

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,15 +1,20 @@
-const API_KEY = "0f50635a0fac9a7da8fdf374a6f46481";
-const BASE_URL = "https://api.themoviedb.org/3"
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const BASE_URL = import.meta.env.VITE_TMDB_BASE_URL;
 
-export const getPopularMovies = async () => {
-    const response= await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
-    const data = await response.json()
-    return data.results
-};
+export async function getPopularMovies() {
+  const res = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  if (!res.ok) throw new Error("Failed to fetch popular movies");
+  const data = await res.json();
+  return data.results;
+}
 
-
-export const searchMovies = async (query) => {
-    const response= await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&{encodeURIComponent(query)}`);
-    const data = await response.json()
-    return data.results
-};
+export async function searchMovies(query) {
+  const res = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  if (!res.ok) throw new Error("Failed to search movies");
+  const data = await res.json();
+  return data.results;
+}
