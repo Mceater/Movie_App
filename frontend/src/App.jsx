e3a88de76138fb9795c08bf784a7b03999a9f2cf
mceater <farhanrisan@gmail.com>
2025-09-07 02:28:07 +1000

    added favorites functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Routes, Route } from 'react-router'
-import MovieCard from './components/MovieCard'
-import Home from './pages/Home'
-import Favorites from './pages/Favorites'
-import NavBar from './components/NavBar'
+import "./App.css";
+import { Routes, Route } from "react-router";
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+import NavBar from "./components/NavBar";
+import { MovieProvider } from "./contexts/MovieContext";
 
 function App() {
-
   return (
-    <div>
+    <MovieProvider>
       <NavBar />
-    <main className='main-content'>
-    <Routes>
-      <Route path='/' element={ <Home /> }/>
-      <Route path='/favorites' element={ <Favorites /> }/>
-    </Routes>
-    </main>
-    </div>
-
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </MovieProvider>
   );
 }
 
-
-export default App
+export default App;
